acaca0067577e99875e987f2ba8d16d932d84670
simone <simonegioele92@gmail.com>
2019-05-22 16:44:03 +0200

    Added MongoDB container

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
-const connectionURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/meme-api';
+const connectionURI = process.env.MONGODB_URI || 'mongodb://mongodb:27017/meme-api';
 const debug = process.env.DEBUG || true;
 
 mongoose.set('debug', debug);
+const connection = mongoose.connect(connectionURI, {useNewUrlParser: true}).then(() => {
+  console.log('Connetected to mongodb');
+}).catch((error) => {
+  throw new Error(error);
+});
 
-export const connection = mongoose.connect(connectionURI, {useNewUrlParser: true});
+export default connection;
 
 
