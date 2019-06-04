33968ad48f8b3f13b62c6727fc1ba98e45adc793
Tristan <ghoststeam217@gmail.com>
2019-06-05 06:06:23 +1200

    Forgot braces... sorry

--- src/models/meme.model.js
+++ src/models/meme.model.js
@@ -1,8 +1,8 @@
 import { model, Schema } from 'mongoose';
 
 export default model('Meme', new Schema({
-  cid: Schema.Types.ObjectId, ref: 'Canvas',
-  uid: Schema.Types.ObjectId, ref: 'User',
+  cid: { type: Schema.Types.ObjectId, ref: 'Canvas' },
+  uid: { type: Schema.Types.ObjectId, ref: 'User' },
   image_path: String, // Only data currently to be stored for the induvidual memes
   stars: Number,
   utc_time: Date,
