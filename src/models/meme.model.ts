43c84c554512e7159c014313adb4e481a3bd7805
Tristan <ghoststeam217@gmail.com>
2019-08-26 22:52:39 +1200

    REMOVED: References to mongodb

--- src/models/meme.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from 'mongoose';
-
-export default model('Meme', new Schema({
-  cid: { type: Schema.Types.ObjectId, ref: 'Canvas' },
-  uid: { type: Schema.Types.ObjectId, ref: 'User' },
-  imagePath: String, // Only data currently to be stored for the induvidual memes
-  stars: Number,
-  utcTime: Date,
-}));
