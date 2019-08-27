2fa71e78a7994d3989f1a04f49437172376f6beb
Tristan <ghoststeam217@gmail.com>
2019-08-27 15:41:18 +1200

    ADDED: Primitive integration of ORM to google authentication

--- src/conf/passport.ts
+++ src/conf/passport.ts
@@ -3,7 +3,7 @@ import Google from 'passport-google-oauth20';
 
 import env from './env';
 
-// import { User } from '../database/entity/user.entity';
+import { User } from '../database/entity/user.entity';
 
 passport.use(
 
@@ -12,6 +12,31 @@ passport.use(
     clientID: env.google_client_id,
     clientSecret: env.google_client_secret
   }, (_accessToken, _refreshToken, profile, done) => {
+
+    User.findOne({googleId: profile.id }).then((user: User) => {
+
+      if (user) {
+
+        console.log('user is:' + user);
+
+      } else {
+
+        const newUser = new User();
+        newUser.googleId = profile.id;
+        newUser.username = profile.displayName;
+        newUser.firstname = profile.name.givenName;
+        newUser.profileImg = (profile.photos[0].value !== undefined ? profile.photos[0].value : 'default_image.jpg');
+        // user.email -> request email in scope
+
+        newUser.save().then((userRecord: User) => {
+
+          console.log('new user is:' + userRecord);
+
+        });
+
+      }
+
+    });
 
     console.log(profile);
     done();
