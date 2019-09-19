f6d06a4ff96b03a7d4534b1262b536e089bce634
Tristan <ghoststeam217@gmail.com>
2019-09-19 16:38:36 +1200

    ADDED: Framework for user search

--- src/routers/api/api.router.ts
+++ src/routers/api/api.router.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+
 import uploadController from './canvas/routes/upload.controller';
 import canvasDocument from './canvas/routes/canvas.document';
+
+import usersController from './search/routes/users.controller';
 
 const router = express.Router();
 
 router.use('/canvas/upload', uploadController);
 router.use('/canvas', canvasDocument);
 router.use('/canvas/image', express.static('uploads/canvas_images')); /** @todo improve image storage when we reach production */
+
+router.use('/search/users', usersController);
 
 export default router;
