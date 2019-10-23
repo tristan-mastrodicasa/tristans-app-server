74e7e50d23ea1f60006a548963fd12dac41e867f
Tristan <ghoststeam217@gmail.com>
2019-10-23 22:50:46 +1300

    ADDED: Draft of landing page / terms / privacy policy

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,8 +1,20 @@
-import { Router } from 'express';
+import express from 'express';
 import apiRouter from './api/api.router';
+import index from './';
+import privacy from './privacy';
+import terms from './terms';
 
-const router = Router();
+const router = express.Router();
 
 router.use('/api', apiRouter);
+
+router.use('/', index.get);
+router.use('/privacy', privacy.get);
+router.use('/terms', terms.get);
+
+router.use('/css', express.static('assets/css'));
+router.use('/img', express.static('assets/img'));
+router.use('/js', express.static('assets/js'));
+router.use('/fonts', express.static('assets/fonts'));
 
 export default router;
