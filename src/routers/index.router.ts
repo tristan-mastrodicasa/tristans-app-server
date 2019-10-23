28a6a46ce23550e466ae744decbec1a13728de1c
Tristan <ghoststeam217@gmail.com>
2019-10-24 10:56:29 +1300

    ADDED: Support page

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -3,6 +3,7 @@ import apiRouter from './api/api.router';
 import index from './';
 import privacy from './privacy';
 import terms from './terms';
+import support from './support';
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.use('/api', apiRouter);
 router.use('/', index.get);
 router.use('/privacy', privacy.get);
 router.use('/terms', terms.get);
+router.use('/support', support.get);
 
 router.use('/css', express.static('assets/css'));
 router.use('/img', express.static('assets/img'));
