d21c6ba30231f341f03dd30ef6403dc41c8a93cd
Tristan <ghoststeam217@gmail.com>
2019-11-30 16:10:46 +1300

    CHANGED: Daily suggestions tests for better coverage

--- src/routers/api/user/document/daily-suggestions/get.spec.ts
+++ src/routers/api/user/document/daily-suggestions/get.spec.ts
@@ -4,7 +4,7 @@ import get from './get';
 import { getNewAuthorizedUser } from 'spec-helpers/authorized-user-setup';
 import { getPhonyCanvas } from 'spec-helpers/phony-canvas-setup';
 import { getPhonyMeme } from 'spec-helpers/phony-meme-setup';
-import { httpErrorMiddleware, runAsyncConcurrently, canvasReactManager } from 'shared/helpers';
+import { httpErrorMiddleware, runAsyncConcurrently, canvasReactManager, memeReactManager } from 'shared/helpers';
 import { ContentCard, EContentType } from 'shared/models';
 
 describe('GET user/:id/daily-suggestions', () => {
@@ -88,7 +88,7 @@ describe('GET user/:id/daily-suggestions', () => {
   });
 
   // Check that if one canvas has more stars than the rest but was made 6 days ago it won't show
-  it('should most starred post should not show if made 6 days ago', async () => {
+  it('should not show most starred post if made 6 days ago', async () => {
     let res: any;
     const bigStar = 10000;
     const userInfo = await getNewAuthorizedUser();
@@ -124,25 +124,45 @@ describe('GET user/:id/daily-suggestions', () => {
 
   });
 
-  // Check if it correctly shows if you starred it //
-  it('should most starred post should not show if made 6 days ago', async () => {
+  it('should show if you starred any of the content', async () => {
     const userInfo = await getNewAuthorizedUser();
     const newCanvas = await getPhonyCanvas(userInfo.user.id);
-
-    // Give the canvas enough stars to show in suggested //
-    newCanvas.stars = 100;
     await newCanvas.save();
 
     // Star your own canvas //
     await canvasReactManager('add', newCanvas.id, userInfo.user.id);
 
+    // Give the canvas enough stars to show in suggested //
+    newCanvas.stars = 100;
+    await newCanvas.save();
+
+    // Create a new meme //
+    const newMemeUser = await getNewAuthorizedUser();
+    const meme = await getPhonyMeme(newCanvas.id, newMemeUser.user.id);
+    await meme.save();
+
+    // Star someone elses meme //
+    await memeReactManager('add', meme.id, userInfo.user.id);
+
+    // Give the meme enough stars to show in suggested //
+    meme.stars = 100;
+    await meme.save();
+
     const res = await supertest(app)
       .get(`/${userInfo.user.id}/daily-suggestions`)
       .set('Authorization', `Bearer ${userInfo.token}`);
 
-    // Should show that you starred your own canvas //
+    console.log(res.body.length);
+
+    // Should show that you starred some content //
     res.body.forEach((value: ContentCard) => {
       if (value.type === EContentType.Canvas && value.id === newCanvas.id) {
+        console.log(value);
+        expect(value.starred).toEqual(true);
+      }
+
+      if (value.type === EContentType.MemeWithHost && value.cid === newCanvas.id) {
+        console.log(value);
         expect(value.starred).toEqual(true);
       }
     });
